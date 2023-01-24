e371545de6d994ec8ce117c4d85088cd50c6af9f
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2023-01-24 15:54:47 +0100

    gallery.js - completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageList = document.querySelector('.gallery');
+
+galleryItems.forEach(image => {
+  imageList.insertAdjacentHTML(
+    'beforeend',
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${image.original}">
+        <img src="${image.preview}" alt="${image.description}" class="gallery__image"/>
+      </a>
+    </li>`
+  );
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
