8494d08fe4246902725ca3c95262dc4dd6d8b3bf
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2023-01-24 16:06:27 +0100

    Task 2 - 100%

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+let currentTime = 0;
+if (localStorage.getItem('videoplayer-current-time') != null) {
+  currentTime = localStorage.getItem('videoplayer-current-time');
+}
+
+player.setCurrentTime(currentTime);
