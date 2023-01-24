cd7cd332d54625792703a8641be338364916fa4a
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2023-01-24 16:44:43 +0100

    Task 3 - 100%

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+const form = document.querySelector('.feedback-form');
+const inputEmail = form.querySelector('label > input');
+const inputText = form.querySelector('label > textarea');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+let throttle = require('lodash.throttle');
 
+form.addEventListener(
+  'input',
+  throttle(() => {
+    const inputValue = {
+      email: inputEmail.value,
+      message: inputText.value,
+    };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputValue));
+  }, 500)
+);
+
+const outputValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+if (localStorage.getItem(LOCALSTORAGE_KEY) === null) {
+  inputEmail.value = '';
+  inputText.value = '';
+} else {
+  inputEmail.value = outputValue.email;
+  inputText.value = outputValue.message;
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+  inputEmail.value = '';
+  inputText.value = '';
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+});
